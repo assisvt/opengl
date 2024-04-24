a6b6d8f03349f0b0d47e02bab1609b0dbd0a490b
Vítor Assis <contatoassisvt@gmail.com>
2024-04-24 10:50:14 -0300

    Update Retangulo.h

--- circulo/Retangulo.h
+++ circulo/Retangulo.h
@@ -1,1 +1,21 @@
+#ifndef RETANGULO_H
+#define RETANGULO_H
+
+
+class Retangulo
+{
+    public:
+        Retangulo(float x, float y, float l, float a);
+        void draw();
+        virtual ~Retangulo();
+        float vx,vy;
+        void update(float deltaTime);
+
+    protected:
+
+    private:
+        float x,y,l,a;
+};
+
+#endif // RETANGULO_H
 
