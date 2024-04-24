a1546270e3566fe4cfeb2194b41f08bb4e8117df
Vítor Assis <contatoassisvt@gmail.com>
2024-04-24 10:49:19 -0300

    Update Circulo.h

--- circulo/Circulo.h
+++ circulo/Circulo.h
@@ -1,1 +1,24 @@
+#ifndef CIRCULO_H
+#define CIRCULO_H
+#include <GL/gl.h>
+
+class Circulo
+{
+    public:
+        Circulo(float Xc, float Yc, float R);
+        void draw();
+        void draw2();
+        virtual ~Circulo();
+        void update();
+        float vx,vy;
+
+
+    protected:
+
+    private:
+        GLfloat centro[3];
+        float R;
+};
+
+#endif // CIRCULO_H
 
